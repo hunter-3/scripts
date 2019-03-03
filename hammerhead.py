ab6adb2f514363344fe2aac0e9cd3dd6bcbcd882
hunter-3 <16930440+hunter-3@users.noreply.github.com>
2019-03-03 16:38:37 -0700

    Update hammerhead.py

--- hammerhead.py
+++ hammerhead.py
@@ -1,21 +1,19 @@
 #!/bin/env python3
 # import os > commands to terminal
-import os
-
 # Merge pcap files > Must run out of directory where pcaps are stored
+# Output > IP.csv using tshark > Distinct count on src ip's
+#  Text file for whois and shodan
+# Creating whois file
+# Display Folder contents > Not needed > check all files are created when completed
+
+import os
 
 os.system("mergecap *.pcap -w merged.pcap")
 
-# Output > IP.csv using tshark > Distinct count on src ip's
 os.system("tshark -r merged.pcap -T fields -e ip.src | sort | uniq -c | sort -nr > ip.csv")
 
-#  Text file for whois and shodan
 os.system("tshark -r merged.pcap -T fields -e ip.src | sort | uniq | sort -nr > ip.txt")
 
-# Creating whois file
 os.system("for i in `cat ip.txt`; do whois $i >> whois.txt; done")
 
-# Shodan API for running IPs through
-
-# Display Folder contents > Not needed > check all files are created when completed
 os.system("ls -al")
