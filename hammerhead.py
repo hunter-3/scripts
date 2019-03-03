c26b23cbc70d429f9bcabbf0c4b5332e9e3d7524
hunter-3 <16930440+hunter-3@users.noreply.github.com>
2019-03-03 16:25:17 -0700

    Create hammerhead.py

---
+++ hammerhead.py
@@ -0,0 +1,21 @@
+# import os > commands to terminal
+import os
+
+# Merge pcap files
+
+os.system("mergecap *.pcap -w merged.pcap")
+
+# Output > IP.csv using tshark > Distinct count on src ip's
+os.system("tshark -r ddos.pcap -T fields -e ip.src | sort | uniq -c | sort -nr > ddos.csv")
+
+#  Text file for whois and shodan
+#       Command tshark -r ddos.pcap -T fields -e ip.src | uniq > ip.txt
+os.system("tshark -r ddos.pcap -T fields -e ip.src | sort | uniq | sort -nr > ip.txt")
+
+# Creating whois file
+os.system("for i in `cat ip.txt`; do whois $i >> whois.txt; done")
+
+# Shodan API for running IPs through
+
+# Display Folder contents
+os.system("ls -al")# Python modules
