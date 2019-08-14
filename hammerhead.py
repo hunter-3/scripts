d77ed708bcbed5d627ca0004651dea2d0a251f07
hunter-3 <16930440+hunter-3@users.noreply.github.com>
2019-08-14 14:33:46 -0700

    Update hammerhead.py

--- hammerhead.py
+++ hammerhead.py
@@ -1,19 +1,38 @@
 #!/bin/env python3
+# Process pcap files into usable formats
 # import os > commands to terminal
 # Merge pcap files > Must run out of directory where pcaps are stored
 # Output > IP.csv using tshark > Distinct count on src ip's
-#  Text file for whois and shodan
-# Creating whois file
-# Display Folder contents > Not needed > check all files are created when completed
 
 import os
 
-os.system("mergecap *.pcap -w merged.pcap")
 
-os.system("tshark -r merged.pcap -T fields -e ip.src | sort | uniq -c | sort -nr > ip.csv")
+# Merge pcap files
 
-os.system("tshark -r merged.pcap -T fields -e ip.src | sort | uniq | sort -nr > ip.txt")
+os.system("mergecap *.pcap -w ddos.pcap")
 
-os.system("for i in `cat ip.txt`; do whois $i >> whois.txt; done")
+# Process merged pcap file to pull out unique src IP's
 
+os.system("tshark -r ddos.pcap -T fields -e ip.src | sort | uniq -c | sort -nr > ddos.csv")
+
+# Protocol Hierarchy Statistics
+
+os.system("tshark -q -r ddos.pcap -z io,phs > protocol_stats.txt")
+
+# Packet Length breakdown
+
+os.system("tshark -nr ddos.pcap -q -z plen,tree > packet_length.txt")
+
+#   whois and shodan
+#       Command tshark -r ddos.pcap -T fields -e ip.src | uniq > ip.txt
+# os.system("tshark -r ddos.pcap -T fields -e ip.src | sort | uniq | sort -nr > ip.txt")
+# Creating whois file
+# os.system("for i in `cat ip.txt`; do whois $i >> whois.txt; done")
+
+# Splunk Rest API for CSV upload > Don't like creds hard code in the curl command
+#curl -k -u admin:pass https://localhost:8089/servicesNS/admin/search/data/lookup-table-files -d eai:data=/opt/splunk/var/run/splunk/lookup_tmp/lookup-in-staging-dir.csv -d name=lookup.csv
+
+# Shodan API > install python library for shodan > Bulk lookup Corp API key (100 per request)
+
+# View files created
 os.system("ls -al")
